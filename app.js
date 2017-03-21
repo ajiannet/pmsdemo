666cd37455db340e584b86c95b98c56b005ae973
ajiannet <ajiannet@163.com>
2017-03-21 12:59:39 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,95 @@
+﻿var app = (function ($) {
+    var initData = function () {
+        var currentProject = {}, projectTypes = [];
+        for (var index in projects) {
+            var item = projects[index];
+            item.type.id = item.id;
+            if (item.type.active) {
+                currentProject = item;
+            }
+            projectTypes.push(item.type);
+        }
+        $('.project-type table tr').empty().append(
+           $('#project-type-tmpl').tmpl(projectTypes, {
+               getActiveCss: function () {
+                   return this.data.active ? 'active' : '';
+               }
+           }));
+
+        loadData(currentProject);
+        initEvent();
+    };
+
+    var initEvent = function () {
+        $('.project-type-td').on('click', '.project-type-item', function () {
+            var $this = $(this), currentProject = {};
+            $this.closest('tr').find('td').removeClass('active');
+            $this.parent().addClass('active');
+            currentProject = projects.filter(function (item) {
+                return item.id == $this.attr('data-project-type');
+            })[0];
+
+            loadData(currentProject);
+        });
+    };
+
+    var loadData = function (project) {
+        $('.project-total').empty().append($('#project-total-tmpl').tmpl(project.total));
+
+        initChart(project.chart);
+
+        $('.project-grid #project-data').empty().append($('#project-grid-tmpl').tmpl(project.data, {
+            getPercentCss: function () {
+                return this.data.percent >= 40 ? "red" : "green";
+            }
+        }));
+    };
+
+    var initChart = function (data) {
+        var myChart = echarts.init(document.getElementById("e_charts"));
+        var myChartOption = {
+            //title: {
+            //    text: '完成比率',
+            //    textStyle:{
+            //        color: '#222222',
+            //        fontSize: '15px',
+            //        fontFamily: 'Microsoft YaHei'
+            //    }
+            //},
+            grid: {
+                left: '10%',
+                right: '10%',
+                top: '5%',
+                bottom: '20%'
+            },
+            xAxis: {
+                data: ['一月', '二月', '三月', '四月', '五月', '六月'],
+                axisTick: { show: false },
+                axisLine: { show: false }
+            },
+            yAxis: {
+                axisLine: { show: false },
+                axisTick: { show: false },
+                //min: 10,
+                axisLabel: { formatter: '{value}%' },
+                splitNumber: 3
+            },
+            animationDurationUpdate: 1200,
+            series: [{
+                type: 'bar',
+                barWidth: 40,
+                itemStyle: {
+                    normal: {
+                        color: '#008FCE'
+                    }
+                },
+                data: data
+            }]
+        };
+        myChart.setOption(myChartOption);
+    };
+
+    return {
+        init: initData
+    };
+})(jQuery);
---
+++ data.js
@@ -0,0 +1,86 @@
+﻿var projects = [{
+    // project
+    id: 1,
+    // project type
+    type: { name: '全部项目', total: 169, red: 56, yellow: 50, green: 63, active: true }, 
+    // project total
+    total: { amount: '5,331,490,208', percent: 67.86 },
+    // project chart
+    chart: [25, 30, 43, 45, 56, 72],
+    // project data
+    data: [
+               { title: '水环境九期项目建设', num1: 32, num2: 0, num3: 1, percent: 85 },
+               { title: '水环境八期项目建设', num1: 28, num2: 0, num3: 1, percent: 83 },
+               { title: '水环境一期项目建设', num1: 10, num2: 0, num3: 1, percent: 45 },
+               { title: '水环境十三一期项目建设', num1: 1, num2: 0, num3: 1, percent: 41 },
+               { title: '水环境十二一期项目建设', num1: 3, num2: 0, num3: 1, percent: 40 },
+               { title: '水环境五期项目建设', num1: 1, num2: 0, num3: 1, percent: 18 },
+               { title: '水环境二期项目建设', num1: 3, num2: 0, num3: 1, percent: 13 },
+               { title: '水环境三期项目建设', num1: 3, num2: 0, num3: 1, percent: 35 },
+               { title: '水环境十期项目建设', num1: 32, num2: 0, num3: 1, percent: 26 },
+               { title: '水环境六期项目建设', num1: 32, num2: 0, num3: 1, percent: 25 },
+               { title: '水环境七期项目建设', num1: 32, num2: 0, num3: 1, percent: 24 },
+               { title: '水环境四期项目建设', num1: 32, num2: 0, num3: 1, percent: 20 },
+               { title: '水环境十一期项目建设', num1: 32, num2: 0, num3: 1, percent: 15 }
+    ]
+}, {
+    id: 2,
+    type: { name: '水务项目', total: 68, red: 32, yellow: 19, green: 17 },
+    total: { amount: '3,815,268,194', percent: 46.32 },
+    chart: [15, 20, 45, 52, 60, 75],
+    data: [
+               { title: '水环境九期项目建设', num1: 32, num2: 0, num3: 1, percent: 90 },
+               { title: '水环境八期项目建设', num1: 28, num2: 0, num3: 1, percent: 85 },
+               { title: '水环境一期项目建设', num1: 10, num2: 0, num3: 1, percent: 42 },
+               { title: '水环境十三一期项目建设', num1: 1, num2: 0, num3: 1, percent: 45 },
+               { title: '水环境十二一期项目建设', num1: 3, num2: 0, num3: 1, percent: 48 },
+               { title: '水环境五期项目建设', num1: 1, num2: 0, num3: 1, percent: 52 },
+               { title: '水环境二期项目建设', num1: 3, num2: 0, num3: 1, percent: 15 },
+               { title: '水环境三期项目建设', num1: 3, num2: 0, num3: 1, percent: 35 },
+               { title: '水环境十期项目建设', num1: 32, num2: 0, num3: 1, percent: 26 },
+               { title: '水环境六期项目建设', num1: 32, num2: 0, num3: 1, percent: 25 },
+               { title: '水环境七期项目建设', num1: 32, num2: 0, num3: 1, percent: 24 },
+               { title: '水环境四期项目建设', num1: 32, num2: 0, num3: 1, percent: 20 },
+               { title: '水环境十一期项目建设', num1: 32, num2: 0, num3: 1, percent: 15 }
+    ]
+}, {
+    id: 3,
+    type: { name: '水环境项目', total: 46, red: 13, yellow: 10, green: 23 },
+    total: { amount: '1,042,360,468', percent: 56.84 },
+    chart: [20, 30, 36, 42, 60, 82],
+    data: [
+               { title: '水环境九期项目建设', num1: 32, num2: 0, num3: 1, percent: 75 },
+               { title: '水环境八期项目建设', num1: 28, num2: 0, num3: 1, percent: 82 },
+               { title: '水环境一期项目建设', num1: 10, num2: 0, num3: 1, percent: 45 },
+               { title: '水环境十三一期项目建设', num1: 1, num2: 0, num3: 1, percent: 41 },
+               { title: '水环境十二一期项目建设', num1: 3, num2: 0, num3: 1, percent: 40 },
+               { title: '水环境五期项目建设', num1: 1, num2: 0, num3: 1, percent: 18 },
+               { title: '水环境二期项目建设', num1: 3, num2: 0, num3: 1, percent: 13 },
+               { title: '水环境三期项目建设', num1: 3, num2: 0, num3: 1, percent: 35 },
+               { title: '水环境十期项目建设', num1: 32, num2: 0, num3: 1, percent: 26 },
+               { title: '水环境六期项目建设', num1: 32, num2: 0, num3: 1, percent: 25 },
+               { title: '水环境七期项目建设', num1: 32, num2: 0, num3: 1, percent: 24 },
+               { title: '水环境四期项目建设', num1: 32, num2: 0, num3: 1, percent: 20 },
+               { title: '水环境十一期项目建设', num1: 32, num2: 0, num3: 1, percent: 15 }
+    ]
+}, {
+    id: 4,
+    type: { name: '环卫项目', total: 55, red: 11, yellow: 21, green: 23 },
+    total: { amount: '1,127,320,875', percent: 25.34 },
+    chart: [22, 32, 55, 61, 78, 90],
+    data: [
+               { title: '水环境九期项目建设', num1: 32, num2: 0, num3: 1, percent: 86 },
+               { title: '水环境八期项目建设', num1: 28, num2: 0, num3: 1, percent: 82 },
+               { title: '水环境一期项目建设', num1: 10, num2: 0, num3: 1, percent: 42 },
+               { title: '水环境十三一期项目建设', num1: 1, num2: 0, num3: 1, percent: 47 },
+               { title: '水环境十二一期项目建设', num1: 3, num2: 0, num3: 1, percent: 40 },
+               { title: '水环境五期项目建设', num1: 1, num2: 0, num3: 1, percent: 18 },
+               { title: '水环境二期项目建设', num1: 3, num2: 0, num3: 1, percent: 13 },
+               { title: '水环境三期项目建设', num1: 3, num2: 0, num3: 1, percent: 35 },
+               { title: '水环境十期项目建设', num1: 32, num2: 0, num3: 1, percent: 26 },
+               { title: '水环境六期项目建设', num1: 32, num2: 0, num3: 1, percent: 25 },
+               { title: '水环境七期项目建设', num1: 32, num2: 0, num3: 1, percent: 24 },
+               { title: '水环境四期项目建设', num1: 32, num2: 0, num3: 1, percent: 20 },
+               { title: '水环境十一期项目建设', num1: 32, num2: 0, num3: 1, percent: 15 }
+    ]
+}];
